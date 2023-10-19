066a17c9d97259156fef0515edea6e6c26572190
MoeinDeveloper92 <moeinsamani1992@gmail.com>
2023-10-20 00:07:05 +0200

    some changes

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,8 +1,22 @@
 import React from 'react'
 import { FaSignInAlt, FaSignOutAlt, FaUser } from "react-icons/fa"
-import { Link } from 'react-router-dom'
+import { Link, useNavigate } from 'react-router-dom'
+import { useSelector, useDispatch } from 'react-redux'
+import { logout, reset } from '../features/auth/authSlice'
+
 
 const Header = () => {
+    const { user } = useSelector((state) => state.auth)
+    const dispatch = useDispatch()
+    const navigate = useNavigate()
+
+
+    const onLogout = () => {
+        dispatch(logout())
+        dispatch(reset())
+        navigate("/")
+    }
+
     return (
         <header className='header'>
             <div className="logo">
@@ -10,18 +24,27 @@ const Header = () => {
             </div>
 
             <ul>
-                <li>
-                    <Link to={"/login"}>
-                        <FaSignInAlt />Login
-                    </Link>
-                </li>
-                <li>
-                    <Link to={"/register"}>
-                        <FaUser />Register
-                    </Link>
-                </li>
-            </ul>
-        </header>
+                {user ? (<>
+                    <li>
+                        <button className='btn ' onClick={onLogout}>
+                            <FaSignOutAlt />Logout
+                        </button>
+                    </li>
+                </>) : (<>
+                    <li>
+                        <Link to={"/login"}>
+                            <FaSignInAlt />Login
+                        </Link>
+                    </li>
+                    <li>
+                        <Link to={"/register"}>
+                            <FaUser />Register
+                        </Link>
+                    </li>
+                </>)}
+
+            </ul >
+        </header >
     )
 }
 
