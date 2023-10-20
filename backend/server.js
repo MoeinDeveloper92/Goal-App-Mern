54c226278739c0ba0e4a89af1b24a6cc30153c3d
MoeinDeveloper92 <moeinsamani1992@gmail.com>
2023-10-20 20:13:48 +0200

    final change

--- backend/server.js
+++ backend/server.js
@@ -1,4 +1,5 @@
 const express = require("express")
+const path = require("path")
 const colors = require("colors")
 const connectDB = require("./config/db")
 const dotenv = require("dotenv").config()
@@ -15,6 +16,19 @@ app.use(express.urlencoded({ extended: false }))
 app.use("/api/goals", require("./routes/goalRoutes"))
 app.use("/api/users", require("./routes/userRoutes"))
 //below code overrides express error handler
+
+//Serve Frontend
+if (process.env.NODE_ENV === 'production') {
+    //ser our static folder
+    app.use(express.static(path.join(__dirname, "../frontend/build")))
+    app.get("*", (req, res) => {
+        res.sendFile(path.resolve(__dirname, "../", "frontend", "build", "index.html"))
+    })
+} else {
+    app.get("/", (req, res) => {
+        res.send("Please set to production")
+    })
+}
 app.use(errorHandler)
 
 app.listen(PORT, () => {
